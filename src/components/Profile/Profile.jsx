f5a1e4713b9e7364d46ebc083f15d53802ae5c23
AlexAmanitas <104567917+AlexAmanitas@users.noreply.github.com>
2022-11-25 23:23:21 +0200

    fin

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+import {
+  Avatar,
+  Description,
+  Label,
+  Location,
+  Name,
+  ProfileCard,
+  Quantity,
+  Stats,
+  StatsItem,
+  Tag,
+} from './Profile.styled';
+
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <ProfileCard>
+      <Description>
+        <Avatar src={avatar} alt="User avatar" />
+        <Name>{username}</Name>
+        <Tag>@{tag}</Tag>
+        <Location>{location}</Location>
+      </Description>
+
+      <Stats>
+        <StatsItem>
+          <Label>Followers</Label>
+          <Quantity>{stats.followers}</Quantity>
+        </StatsItem>
+        <StatsItem>
+          <Label>Views</Label>
+          <Quantity>{stats.views}</Quantity>
+        </StatsItem>
+        <StatsItem>
+          <Label>Likes</Label>
+          <Quantity>{stats.likes}</Quantity>
+        </StatsItem>
+      </Stats>
+    </ProfileCard>
+  );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.object,
+};
